d7e4b0a5fea38ffe9b410b056ec4835286edca0a
dbsrlehs92@gmail.com <dbsrlehs92@gmail.com>
2021-03-09 23:29:31 +0900

    랜덤 초에 스케쥴링 하도록 수정

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+"use strict";
+const config = require("config");
+const sleep = require("sleep");
 const { scheduler } = require("./index");
-const schedule = require('node-schedule');
-const config = require('config');
+const { Random } = require("./util");
+const logger = require("./logger").getLogger(module);
 
 (async () => {
-  /* 공시 정보를 7초에 한번씩 탐색 */
-  schedule.scheduleJob(config.cronSchedule, async function() {
+  logger.info("Start crypto-notice-watcher application.");
+  logger.info(JSON.stringify(config, null, 2));
+  /* 공시 정보를 3~10초에 한번씩 랜덤으로 탐색 */
+  while (true) {
     await scheduler();
-  });
+    
+    const randomSecond = Random.getRandomSecond();
+    sleep.sleep(randomSecond);
+  }
+  
 })();
